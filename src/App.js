2e028278a664eedf48f7d4e91f82f8d176abb965
David Palacio <davidpala@microsoft.com>
2022-08-31 20:56:09 -0500

    added input for time

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 const TIME_TO_REFRESH = 3 * 60; // min * sec
 const TARGET_URL = "https://microsoft.com/devicelogin";
@@ -8,34 +8,53 @@ const TARGET_URL = "https://microsoft.com/devicelogin";
 function App() {
   const [isExtensionOn, setIsExtensionOn] = useState(null);
   const [tabId, setTabId] = useState(null);
+  const [userTimeToRefresh, setUserTimeToRefresh] = useState(TIME_TO_REFRESH);
+  const [inputUserTimeToRefreshValue, setInputUserTimeToRefreshValue] = useState(TIME_TO_REFRESH);
 
   // Load current tab id
-  chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
-    setTabId(tabs[0].id);
-  });
+  useEffect(() => {
+    chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
+      setTabId(tabs[0].id);
+    });
+  }, []);
 
   // To figure out the state the buttons should be
-  chrome.runtime.sendMessage({
-    command: "isEnabled",
-    tabId: tabId
-  }, isEnabled => {
-    setIsExtensionOn(isEnabled);
-  });
+  useEffect(() => {
+    chrome.runtime.sendMessage({
+      command: "isEnabled",
+      tabId: tabId
+    }, isEnabled => {
+      setIsExtensionOn(isEnabled);
+    });
+  }, []);
+
+  // To keep alive the connection between the extension and the content script
+  useEffect(() => {
+    if (!tabId) {
+      return;
+    }
+
+    const interval = setInterval(() => {
+      sentKeepAlive();
+    }, 10 * 1000);
+
+    return () => clearInterval(interval);
+  }, [tabId]);
 
   // Methods
 
-  function enableRedireactionForCurrentTab() {
+  function enableRedirectionForCurrentTab() {
     chrome.runtime.sendMessage({
       command: "enable",
       tabId: tabId,
-      timeToRefresh: TIME_TO_REFRESH,
+      timeToRefresh: userTimeToRefresh,
       targetUrl: TARGET_URL
     });
 
     setIsExtensionOn(true);
   }
 
-  function disableRedireactionForCurrentTab () {
+  function disableRedirectionForCurrentTab() {
     chrome.runtime.sendMessage({
       command: "disable",
       tabId: tabId
@@ -44,24 +63,37 @@ function App() {
     setIsExtensionOn(false);
   }
 
-  chrome.runtime.onMessage.addListener(message => {
-    chrome.tabs.sendMessage(message.tabId, { targetUrl: message.targetUrl });
-  });
+
+  function onUserTimeToRefreshInputChange(event) {
+    setInputUserTimeToRefreshValue(event.target.value);
+
+    const parsedValue = Number.parseInt(event.target.value);
+
+    if (Number.isInteger(parsedValue)) {
+      setUserTimeToRefresh(parsedValue)
+    }
+  }
 
   return (
     <>
       <button 
         type='button'
         disabled={isExtensionOn === true}
-        onClick={enableRedireactionForCurrentTab}>Enable</button>
+        onClick={enableRedirectionForCurrentTab}>Enable</button>
       <button
         type='button'
         class="margin-top"
-        onClick={disableRedireactionForCurrentTab} 
+        onClick={disableRedirectionForCurrentTab} 
         disabled={!isExtensionOn}
         >Disable</button>
+
+        <input type="text" placeholder='Enter seconds' className='margin-top' value={inputUserTimeToRefreshValue} onChange={onUserTimeToRefreshInputChange} />
     </>
   );
 }
+
+chrome.runtime.onMessage.addListener(message => {
+  chrome.tabs.sendMessage(message.tabId, message);
+});
 
 export default App;
